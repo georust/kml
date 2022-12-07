b1c6957f142d1815ce4754585a03e29a6e516fd7
Zack Dannar <zack.dannar@onxmaps.com>
2022-12-07 13:50:01 -0700

    update

--- src/writer.rs
+++ src/writer.rs
@@ -145,10 +145,6 @@ where
         }
         self.writer
             .write_event(Event::End(BytesEnd::owned(b"data".to_vec())))?;
-        /*
-        let start = BytesStart::borrowed_name(e.name.as_bytes())
-         .with_attributes(self.hash_map_as_attrs(&e.attrs));
-         */
         self.write_text_element(b"extrude", if point.extrude { "1" } else { "0" })?;
         self.write_text_element(b"altitudeMode", &point.altitude_mode.to_string())?;
         self.write_text_element(b"coordinates", &point.coord.to_string())?;
